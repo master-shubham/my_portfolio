120d41acea198146dec4d818848506e7619167a1
Master Shubham <ommylight@gmail.com>
2025-12-29 18:50:39 +0530

    Create layout of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
-import Hero from './components/Hero'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 
 function App() {
 
   return (
-    <>
-      <Navbar/>
-      <Hero/>
-    </>
+     <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
